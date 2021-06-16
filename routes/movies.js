a586b0dd199133d9e881243edf83c345d310e322
Nicolas Vargas <ripperoffire@gmail.com>
2021-06-15 20:56:32 -0400

    Conexión a MongoAtlas una instancia de MongoDB

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const { moviesMock } = require("../utils/mocks/movies");
+const MoviesService = require("../services/movies");
 
 function moviesApi(app) {
     const router = express.Router();
     app.use("/api/movies", router);
 
+    const moviesService = new MoviesService();
+
     router.get('/', async function(req, res, next){
+        const { tags } = req.query;
+
         try {
-            const movies = await Promise.resolve(moviesMock);
+            const movies = await moviesService.getMovies({ tags })
 
             res.status(200).json({ 
                 data: movies,
@@ -19,8 +23,10 @@ function moviesApi(app) {
     });
 
     router.get('/:movieId', async function(req, res, next){
+        const { movieId } = req.params;
+        
         try {
-            const movies = await Promise.resolve(moviesMock[0]);
+            const movies = await moviesService.getMovie({ movieId });
 
             res.status(200).json({ 
                 data: movies,
@@ -32,8 +38,11 @@ function moviesApi(app) {
     });
 
     router.post('/', async function(req, res, next){
+        
+        const { body: movie } = req;
+        
         try {
-            const createMovieId = await Promise.resolve(moviesMock[0].id);
+            const createMovieId = await moviesService.createMovie({ movie });
 
             res.status(201).json({ 
                 data: createMovieId,
@@ -45,8 +54,14 @@ function moviesApi(app) {
     });
 
     router.put('/:movieId', async function(req, res, next){
+        const { movieId } = req.params;
+        const { body: movie } = req;
+        
         try {
-            const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+            const updatedMovieId = await moviesService.updateMovie({
+                movieId, 
+                movie 
+            });
 
             res.status(200).json({ 
                 data: updatedMovieId ,
@@ -58,14 +73,36 @@ function moviesApi(app) {
     });
 
     router.delete('/:movieId', async function(req, res, next){
+        const { movieId } = req.params;
+        
         try {
-            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+            const deletedMovieId = await moviesService.deleteMovie({ movieId });
 
             res.status(200).json({ 
                 data: deletedMovieId ,
                 message: 'movies deleted'
             });
         } catch(err) {
+            next(err);
+        }
+    });
+    //reto
+    // creamos nuestro metodo patch en el router de nuestro servidor recibiendo el id de la movie
+    router.patch('/:movieId', async (req, res, next) => {
+        // destructuramos el parametro movieID que viene de la ruta
+        const { movieId } = req.params;
+        // recibimos los datos que vienen de body y colocamos un alias "dataMovie"
+        const { body: dataMovie } = req;
+        try{
+            // llamamos al servicio patchMovie para que se encargue de la lógica a implementar
+            const patchMovie = await moviesService.patchMovie({ movieId, dataMovie });
+            // enviamos la respuesta exitosa al servidor
+            res.status(200).json({
+                data: patchMovie,
+                message: 'updated movie'
+            });
+        } catch(err) {
+            // en caso de error lo capturamos error
             next(err);
         }
     });
