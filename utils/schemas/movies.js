f132a59c09c35e8c6686ac48ae7eafb0b61a0245
Nicolas Vargas <vargastapianicolas@gmail.com>
2021-06-29 21:55:51 -0400

    Validacion de nuestros endpoints

--- utils/schemas/movies.js
+++ utils/schemas/movies.js
@@ -1,13 +1,14 @@
 const joi = require('@hapi/joi');
 
+
 const movieIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const movieTitleSchema = joi.string().max(80);
 const movieYearSchema = joi.number().min(1888).max(2077);
-const movieCoverSchema = joi.string().url();
+const movieCoverSchema = joi.string().uri();
 const movieDescriptionSchema = joi.string().max(300);
-const movieDurationSchema = joi.string().min(1).max(300);
+const movieDurationSchema = joi.number().min(1).max(300);
 const movieContentRatingSchema = joi.string().max(5);
-const movieSourceSchema = joi.string().url();
+const movieSourceSchema = joi.string().uri();
 const movieTagsSchema = joi.array().items(joi.string().max(50));
 
 const createMovieSchema = {
